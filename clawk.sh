f6aec94555587b6876f663c98783b7fb8881b51f
Haitham Aouati <55059470+haithamaouati@users.noreply.github.com>
2026-05-24 07:54:17 +0100

    Update clawk.sh

--- clawk.sh
+++ clawk.sh
@@ -140,6 +140,6 @@ if [[ -n $id ]]; then
     echo -e "TikTok profile: ${underline}https://tiktok.com/@$uniqueId${nc}\n"
 else
     echo -e "\nFailed to fetch account details. TikTok might block the request or username doesn't exist.\n"
-    echo -e "Try webapp: ${underline}haithamaouati.github.io/clawkscraper$nc\n"
+    echo -e "Try webapp: ${underline}haithamaouati.github.io/TikToker$nc\n"
     exit 1
 fi
